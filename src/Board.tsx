db833ec271d9c8f418673dd692ce482047033bc4
atsuki-t <atsuki-t@weseek.co.jp>
2021-07-04 05:15:51 +0900

    show winner

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,19 +2,43 @@ import React, { useState } from 'react';
 import Square from './Square';
 
 const Board: React.FC = () => {
-  // let squares: ('X' | 'O' | '')[] = Array(9).fill('');
-  const [squares, setSquares] = useState(Array(9).fill(''))
+  const [squares, setSquares] = useState(Array(9).fill(''));
+  const [xIsNext, setxIsNext] = useState(false);
 
   const renderSquare = (number: number) => {
     return <Square circleAndCross={squares[number]} onClick={() => handleClick(number)} />
   }
 
   const handleClick = (i: number) => {
-    squares[i] = 'X';
-    setSquares(squares);
+    if (calculateWinner(squares) == null) {
+      const newSquares = squares.slice();
+      newSquares[i] = xIsNext ? 'X' : 'O';
+      setxIsNext(!xIsNext);
+      setSquares(newSquares);
+    }
   }
-
-  let status: string = 'Next player: X';
+  
+  const calculateWinner = (squares: string[]) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
+  const status: string = calculateWinner(squares) ? `Winner: ${calculateWinner(squares)}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
   return (
     <div>
